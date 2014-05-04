22f294da42e311febcde7a473554b76371ce2a60
Kurt J. Strosahl <kjstrosahl@gmail.com>
2014-05-04 11:57:43 -0400

    incorporated nosetests compatability 	modified:   logger.py

--- logger.py
+++ logger.py
@@ -16,7 +16,7 @@ def logWrite(logFile, message, head=''):
 
         timestamp = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
-        if os.path.exists(logFile):
+        if os.path.isfile(logFile):
 		try:
 	                log = file(logFile, "a")
 		except IOError:
@@ -29,7 +29,7 @@ def logWrite(logFile, message, head=''):
 			print "cannot open " + logFile + " for writting!"
 			return False
 
-        logMessage = head + ' ' + timestamp + ' ' + message + '\n'
+        logMessage = head + ' ' + timestamp + ' ' + message + '\n' 
 
 	try:
 	        log.write(logMessage)
@@ -40,43 +40,88 @@ def logWrite(logFile, message, head=''):
 
         log.close
 
+	#log write successful
         return True
 
-if __name__ == "__main__":
-
+def testLogWrite():
+	"""
+	Runs a test of the logwrite function
+	First it tests creating and writting the logfile
+	Then it tests appending the logfile.
+	Logs are written into the same directory as logger.py
+	"""
 
 	logfile = 'testwrite.log'
-	fail = "Test failed"
 
 	print "tesing logger modules\n"
 
+	#checks to see if there are any lingering testlogs and removes them
+	if os.path.isfile(logfile): 
+		try:
+			print "old test log found, trying to remove"
+			os.remove(logfile)
+		except OSError:
+			assert False
+			return False
+
+	#tests creation and initial write of the logfile
 	print "testing write of a logfile (will overwrite existing test logwrite)..."
 	retur = logWrite(logfile, "this is a test write")
 	print "return from test write: " + str(retur)
 	if not retur:
 		print "write failed, exiting test"
-		sys.exit(fail)
+		assert False
+		return False		
 
+	#verifies that the file can be appended
 	print "testing log file append (appends to prior created log file)..."
 	retur = logWrite(logfile, "this is a test append")
 	print "return from test append: " + str(retur)
 	if not retur:
 		print "append failed, exiting test"
-		sys.exit(fail)
+		assert False
+		return False		
 
+	#verifies that the file was created
 	print "\nVerifying creation files..."
-	if os.path.exists(logfile):
+	if os.path.isfile(logfile):
 		print "log file found: " + logfile
 	else:
 		print "log file not found: " + logfile
-		sys.exit(fail)		
+		assert False
+		return False		
 
+	#for visual inspection
 	print "\nVerifying file contents..."
 	print "expected contents:"
 	print "<timestamp> this is a test write"
 	print "<timestamp> this is a test append"
 
-	file = open(logfile, 'r')
-	for line in file:
+	#print contents of temp logfile and calculate number of lines
+	f = open(logfile, 'r')
+	count = 0
+	for line in f:
 		print line
+		count = count + 1
+	f.close
 
+	#check to ensure that only two lines were writting to the log
+	if count != 2:
+		assert False
+		return False
+
+	#remove temporary log file
+	try:
+		os.remove(logfile)
+	except OSError:
+		assert False
+		return False
+
+	assert True
+	return True	
+
+if __name__ == "__main__":
+	"""
+	If run as a standalone it runs the logwrite test
+	"""
+	testLogWrite()
